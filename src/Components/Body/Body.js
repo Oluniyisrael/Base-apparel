4279128d095dcf70795648810d538e00b751865e
unknown <israeloluniyi2004@gmail.com>
2022-08-11 10:54:49 +0100

    sixth commit

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,12 +3,14 @@ import './Body.css'
 import hero from '../../images/hero-desktop.jpg'
 import  logo from'../../images/logo.svg'
 import arrow from'../../images/icon-arrow.svg'
+import herom from '../../images/hero-mobile.jpg'
 
 function Body() {
   return (
       <div id='body'>
           <div id='left'>
               <img src={logo} alt='...'/>
+              <div ><img src={herom} alt=',...' id='mobileimg'/></div>
               <div id='textcontainer'>
                 <span id='come'><span class='purple'>WE'RE </span><br/>COMING SOON</span>
                 <p class='purple' id='hello' >Hello shoppers! We are currently building our new fashion store. Add your Email below to stay up-to-date with annoucements and our launch deals</p> 
